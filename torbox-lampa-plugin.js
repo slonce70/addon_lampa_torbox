caeb6d0ed200595eca315019683e9358c90d1fff
slonce70 <130596182+slonce70@users.noreply.github.com>
2025-06-25 19:31:28 +0300

    Update torbox-lampa-plugin.js

--- torbox-lampa-plugin.js
+++ torbox-lampa-plugin.js
@@ -85,7 +85,7 @@
         const key = Store.get('torbox_api_key', '');
         if (!key) throw new Error('API-Key не указан.');
         
-        const url = `${this.SEARCH_API}/torrents/imdb:${imdbId}?check_cache=true&check_owned=false&search_user_engines=true`;
+        const url = `${this.SEARCH_API}/torrents/imdb:${imdbId}?check_cache=true&check_owned=false&search_user_engines=false`;
         
         const options = { headers: { 'Authorization': `Bearer ${key}` } };
         const res = await this.proxiedCall(url, options);
